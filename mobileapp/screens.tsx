e7b7bc72bd448fcc313a15997822215629d84b7d
RehanTaneja <rehan.taneja4321@gmail.com>
2026-04-04 21:13:44 -0400

    Time parsing changed, fixed dashboard timer for trial diet

--- mobileapp/screens.tsx
+++ mobileapp/screens.tsx
@@ -1311,7 +1311,9 @@ const DashboardScreen = ({ navigation, route }: { navigation: any, route?: any }
           const subscriptionStatus = await getSubscriptionStatus(userId);
           
           if (subscriptionStatus.isTrialActive && subscriptionStatus.trialEndDate) {
-            const trialEndDate = new Date(subscriptionStatus.trialEndDate);
+            const rawTrialEndDate = subscriptionStatus.trialEndDate;
+            const hasTimezone = /(?:Z|[+\-]\d{2}:\d{2})$/i.test(rawTrialEndDate);
+            const trialEndDate = new Date(hasTimezone ? rawTrialEndDate : `${rawTrialEndDate}Z`);
             const now = new Date();
             const diffMs = trialEndDate.getTime() - now.getTime();
             
